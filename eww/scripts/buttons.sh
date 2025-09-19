5786531ac686e64b61aa3ee663042cc34a7542e9
odextrs <odextrs@gmail.com>
2025-09-18 23:00:12 -0300

    eww

---
+++ eww/scripts/buttons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ "$1" == "--dolphin" ]]; then
+	dolphin &
+
+elif [[ "$1" == "--lock" ]]; then
+	hyprlock &
+
+elif [[ "$1" == "--wofi" ]]; then
+	vicinae &
+
+elif [[ "$1" == "--hyprshot" ]]; then
+	hyprshot -m region &
+
+elif [[ "$1" == "--audio" ]]; then
+        flatpak run com.saivert.pwvucontrol &
+
+fi
