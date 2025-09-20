b4425cde1588b0ee142f8e69da1e143ab394d25d
odextrs <odextrs@gmail.com>
2025-09-20 12:57:17 -0300

    Update eww.sh
    open wallpaper and powerMenu

--- config/hypr/scripts/eww.sh
+++ config/hypr/scripts/eww.sh
@@ -1,3 +1,5 @@
 eww -c ~/.config/nuno-shell/ open desktop
+eww -c ~/.config/nuno-shell/ open wallpaper
 eww -c ~/.config/nuno-shell/ open leftBar
 eww -c ~/.config/nuno-shell/ open rightSide
+eww -c ~/.config/nuno-shell/ open powerMenu
